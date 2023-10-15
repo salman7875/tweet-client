7a7e05e479b3fc66bf03ea321eaf83643b100248
imsajid12 <imsajid12@gmail.com>
2023-10-15 12:11:56 +0530

    url

--- src/Utils/request.js
+++ src/Utils/request.js
@@ -1,2 +1,2 @@
-export const localEndPoint = "http://localhost:5000/api";
-export const hostEndPoint = "https://tweet-spot.onrender.com/api"
+// export const localEndPoint = "http://localhost:5000/api";
+export const localEndPoint = "https://tweet-spot.onrender.com/api"
